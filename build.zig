2f74e39c29e4f3ab5f77b634f6dcea5bf0798267
scss28 <83139747+scss28@users.noreply.github.com>
2024-11-18 17:01:24 +0100

    Initial commit

---
+++ build.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "raylib-example",
+        .target = target,
+        .link_libc = true,
+        .optimize = optimize,
+    });
+
+    exe.addCSourceFile(.{ .file = b.path("src/main.cpp") });
+    exe.addIncludePath(b.path("include"));
+
+    const raylib = b.dependency("raylib", .{});
+    exe.linkLibrary(raylib.artifact("raylib"));
+    exe.linkLibCpp();
+
+    const run = b.addRunArtifact(exe);
+    b.step("run", "Run the app").dependOn(&run.step);
+}
